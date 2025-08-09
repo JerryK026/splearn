af4e11f1ac01e09331f8a17e9517588810cd8e8e
SeokyungKim <kskyung0624@gmail.com>
2025-08-10 08:03:03 +0900

    feat: Member 도메인 모델 확장

--- src/main/kotlin/tobyspring/splearn/domain/Member.kt
+++ src/main/kotlin/tobyspring/splearn/domain/Member.kt
@@ -1,22 +1,40 @@
 package tobyspring.splearn.domain
 
 
-class Member(
+class Member private constructor(
     val email: String,
-    val nickname: String,
-    val passwordHsh: String
+    var nickname: String,
+    var passwordHsh: String
 ) {
     var status: MemberStatus = MemberStatus.PENDING
 
-    fun activate() {
+    companion object {
+        fun create(email: String, nickname: String, password: String, passwordEncoder: PasswordEncoder): Member {
+            return Member(email, nickname, passwordEncoder.encode(password))
+        }
+    }
+
+    fun activate(): Unit {
         check(this.status == MemberStatus.PENDING) { "PENDING 상태가 아닙니다" }
 
         status = MemberStatus.ACTIVE
     }
 
-    fun deactivate() {
+    fun deactivate(): Unit {
         check(this.status == MemberStatus.ACTIVE) { "ACTIVE 상태가 아닙니다" }
 
         this.status = MemberStatus.DEACTIVATED
+    }
+
+    fun verifyPassword(password: String, passwordEncoder: PasswordEncoder): Boolean {
+        return passwordEncoder.matches(password, this.passwordHsh)
+    }
+
+    fun changeNickname(nickname: String) {
+        this.nickname = nickname
+    }
+
+    fun changePassword(password: String, passwordEncoder: PasswordEncoder) {
+        this.passwordHsh = passwordEncoder.encode(password)
     }
 }
